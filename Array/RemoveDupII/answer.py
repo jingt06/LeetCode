26684b6b98cc430d309d2a9f5192c7f7996b1e47
Jing Tao <genetao06@hotmail.com>
2015-12-28 23:36:54 -0500

    update

--- Array/RemoveDupII/answer.py
+++ Array/RemoveDupII/answer.py
@@ -4,9 +4,15 @@ import sys;
 arr =  input("give a list of sorted array\n").split(" ")
 arr = list(filter(None, arr))
 prev = None # keep track of the previous number
+count = 0 # count how many times a number is duplicate
 new_arr = []
 for x in arr:
 	if x != prev:
-		prev = x;
+		prev = x
 		new_arr.append(x)
+		count = 0
+	else:
+		count = count + 1
+		if(count <= 1):
+			new_arr.append(x)
 print(new_arr);
