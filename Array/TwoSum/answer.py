db89db1eedce0911f7e4033801d47aefc7923244
Jing Tao <genetao06@hotmail.com>
2016-01-02 23:14:37 -0500

    Two Sum

--- Array/TwoSum/answer.py
+++ Array/TwoSum/answer.py
@@ -8,10 +8,8 @@ def TwoSum(list, target):
 		find = hashmap.get(target - key) 
 		if find is not None:
 			print("%d %d" % (find, count))
-			return
 		else:
 			hashmap[key] = count
 			count = count + 1
-	print(-1)
 
-TwoSum([1,2,7,3,10], 9)
+TwoSum([1,2,7,3,10,8], 9)
