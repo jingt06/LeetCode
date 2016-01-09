054a8a15ff7d55264de4c846d4b8849562b75683
Jing Tao <genetao06@hotmail.com>
2016-01-08 23:05:20 -0500

    typo

--- Array/2SumSorted/answer.py
+++ Array/2SumSorted/answer.py
@@ -4,7 +4,7 @@ def TwoSumSorted(List, sum):
 	i = 0
 	j = len(list) - 1
 	while i != j:
-		if List[i] + List[j] = sum:
+		if List[i] + List[j] == sum:
 			print("%d %d" % List[i], List[j])
 			return
 		elif List[i] + List[j] > sum:
