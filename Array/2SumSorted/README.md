f4e517ebcbe24536e32be6b5fc15732c48a8df7b
Jing Tao <genetao06@hotmail.com>
2016-01-08 22:33:07 -0500

    two sum sorted array

---
+++ Array/2SumSorted/README.md
@@ -0,0 +1,1 @@
+Given an array of sorted integers, find two number such that they add up to a specific target number.
---
+++ Array/2SumSorted/answer.py
@@ -0,0 +1,14 @@
+
+#TwoSumSorted(list of sorted integers, target sum)
+def TwoSumSorted(List, sum):
+	i = 0
+	j = len(list) - 1
+	while i != j:
+		if List[i] + List[j] = sum:
+			print("%d %d" % List[i], List[j])
+			return
+		elif List[i] + List[j] > sum:
+			j = j - 1
+		else:
+			i = i + 1 
+	print("None")
